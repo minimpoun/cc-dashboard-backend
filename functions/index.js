c3621b61ff365926bf06431e9ac2c2fa5b259400
minimpoun <cdringenberg1@gmail.com>
2020-12-21 16:38:29 -0600

    finished CRUD implementation of backend

--- functions/index.js
+++ functions/index.js
@@ -1,40 +1,65 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const Router = require('express');
+const Cors = require('cors');
 
 const rt = Router();
 admin.initializeApp();
 const db = admin.firestore();
 
-async function testGetAirmen(db) {
-  let snapshot = await db.collection('airmen').get();
-  snapshot.forEach(doc => {
-    console.log(doc.id, '=>', doc.data());
-  })
-}
+rt.use(Cors());
 
-//async function getAllAirmen(req, res) {
-//  let snapshot = await db.collection('airmen').get().then(out => {return {result: true, data: out}});
-//  let output = [];
-//
-//  if (snapshot.result) {
-//    snapshot.data.forEach(airman => {
-//      output.push({
-//        id: airman.id,
-//        adls: airman.get('adls'),
-//        evaluation: airman.get('evaluation'),
-//        firstName: airman.get('firstName'),
-//        lastName: airman.get('lastName'),
-//        flightName: airman.get("flightName"),
-//        medical: airman.get('medical')
-//      })
-//    })
-//    res.status(200).send({result: true, data: output});
-//  }
-//  else
-//  {
-//    res.status(400).send({result: false, message: "sad face"});
-//  }
-//}
-//
-//rt.get('/', (req, res) => getAllAirmen(req, res));
+exports.getFlightCodes = functions.https.onRequest(async (req, res) => {
+  const collection = db.collection('squadron');
+  const snapshot = collection.get();
+
+  if ((await snapshot).empty) {
+    res.status(400).send({result: false, message: "data does not exist"});
+  }
+  let output = [];
+  (await snapshot).forEach(async doc => {
+    output.push(doc.id);
+  })
+
+  res.status(200).send({flights: output});
+}) 
+
+rt.get('/:id', async(req, res) => {
+  const collectionRef = await db.collection('squadron');
+  const airmenCollection = await collectionRef.doc(req.params.id).collection('airmen').get();
+
+  let output = [];
+  airmenCollection.forEach(async airman => {
+    output.push({airmanId: airman.id, airmanData: airman.data()});
+  })
+
+  res.status(200).send({ result: output});
+})
+
+exports.getAirmen = functions.https.onRequest(rt);
+
+rt.post('/:id', async (req, res) => {
+  const newAirman = req.body;
+  await db.collection('squadron').doc(req.params.id).collection('airmen').add(newAirman);
+  res.status(201).send({result: "User added succefully"});
+})
+
+exports.addAirman = functions.https.onRequest(rt);
+
+rt.put('/:flight/:id', async (req, res) => {
+  const updateAirman = req.body;
+
+  await db.collection('squadron').doc(req.params.flight).collection('airmen').doc(req.params.id).update(updateAirman);
+
+  res.status(200).send({ result: true, message: "Update completed!"})
+})
+
+exports.updateAirman = functions.https.onRequest(rt);
+
+rt.delete('/:flight/:id', async (req, res) => {
+  await db.collection('squadron').doc(req.params.flight).collection('airmen').doc(req.params.id).delete();
+
+  res.status(200).send({ result: true, message: "User deleted successfully"})
+})
+
+exports.deleteAirman = functions.https.onRequest(rt);
